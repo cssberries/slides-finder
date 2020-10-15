a8d002e782a0f7eb73207d9753a26d171492d2ba
Yevgeniy Belov <ugenmail@gmail.com>
2020-10-15 13:19:37 +0300

    renamed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to blueberry-finder!');
+    expect(page.getTitleText()).toEqual('Welcome to slides-finder!');
   });
 
   afterEach(async () => {
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const updateTree = require( 'slides-server/tasks/update-tree' );
-const server = require( 'slides-server' );
+const updateTree = require( './server/tasks/update-tree' );
+const server = require( './server' );
+const configs = require('./server/tasks/configs.js');
 const npm = require( "npm" );
 module.exports = {
     setOptions: function ( options ) {
         options['slidesTreePath'] = `${__dirname}/tree.json`;
         options['routesListPath'] = `${__dirname}/routeList.json`;
-        options['slidesListModulePathRender'] = 'node_modules/blueberry-render/src/app/slides/slides-list.module.ts';
-        options['slidesListModulePathFinder'] = 'node_modules/blueberry-finder/src/app/slides/slides-list.module.ts';
+        options['slidesListModulePathRender'] = 'node_modules/slides-render-angular/src/app/slides/slides-list.module.ts';
+        options['slidesListModulePathFinder'] = 'node_modules/slides-finder/src/app/slides/slides-list.module.ts';
         options['slidesListingComponentPath'] = `.`;
         options['slidesRenderComponentPath'] = `.`;
         options['slidesDir'] = 'mockups';
+        options['DOMAIN_NAME'] = options.DOMAIN_NAME;
         process.env['ANGULAR_PORT'] = options.ANGULAR_PORT;
         process.env['SNAPSHOTS_PORT'] = options.SNAPSHOTS_PORT;
         process.env['SERVER_PORT'] = options.SERVER_PORT;
         process.env['BASE_HREF_FINDER'] = options.BASE_HREF_FINDER;
-        process.env['DEPLOY_REPO'] = options.DEPLOY_REPO;
+        process.env['GH_REPOSITORY_FINDER'] = options.GH_REPOSITORY_FINDER;        
     },
 
     run: function ( options ) {
@@ -27,6 +29,7 @@ module.exports = {
     },
 
     deploy: function ( options ) {
+        configs.setEnvProd(options);
         this.setOptions( options );
         npm.load( () => {
             npm.run( 'deployFinder' );
