f243358acd080adc282d29c8aa5d8f9c1a93e856
Yevgeniy Belov <ugenmail@gmail.com>
2020-10-20 09:10:34 +0300

    Added component selector to new slides.

--- server/tasks/new-slide.js
+++ server/tasks/new-slide.js
@@ -11,6 +11,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
+    selector: '${slideName}',
     templateUrl: 'template.html',
     
     })
