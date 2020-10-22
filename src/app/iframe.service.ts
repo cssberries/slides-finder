17282a9f88150d54605498a09133f0331a2aee5f
Yevgeniy Belov <ugenmail@gmail.com>
2020-10-22 10:13:28 +0300

    formating

--- src/app/iframe.service.ts
+++ src/app/iframe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+   import { Injectable } from '@angular/core';
 import { LayoutService } from './layout.service';
 import { StorageService } from './storage.service';
 
