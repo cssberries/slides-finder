40533132f2f66af9c6d24715de48bb115c0f3741
Yevgeniy Belov <ugenmail@gmail.com>
2020-10-22 10:15:08 +0300

    extracted routes to a const.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+const routs: Routes = [
+	{
+		path: 'slides',
+		loadChildren: () =>
+			import('./slides/slides.module').then(
+				(m) => m.SlidesModule
+			),
+	},
+	{ path: '', redirectTo: 'projects', pathMatch: 'full' },
+]
+
 
 @NgModule({
 	imports: [
-		RouterModule.forRoot([
-			{
-				path: 'slides',
-				loadChildren: () =>
-					import('./slides/slides.module').then(
-						(m) => m.SlidesModule
-					),
-			},
-			{ path: '', redirectTo: 'slides/mockups', pathMatch: 'full' },
-		]),
-	],
-	exports: [RouterModule],
+		RouterModule.forRoot(routs)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule {}
+
+export class AppRoutingModule { }
